5fe93df0d48e23acd23dde8cc284b86a0bcd40fe
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-03-12 01:21:03 +0200

    Make admin panel available

--- odin/urls.py
+++ odin/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
     
     url(r'', include('website.urls')),
 )
