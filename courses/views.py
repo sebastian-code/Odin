cc1528ad050235e7e699219eeaa88edb73914e67
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-03-12 23:15:14 +0200

    Add show all courses

--- courses/views.py
+++ courses/views.py
@@ -6,3 +6,9 @@ def show_course(request, course_id):
     course = get_object_or_404(Course, id=course_id)
 
     return render(request, "show_course.html", locals())
+
+
+def show_all_courses(request):
+    courses = Course.objects.all()
+
+    return render(request, "show_all_courses.html", locals())
