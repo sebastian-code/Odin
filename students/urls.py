a17beb9efed5b81b8ebdbf7465124d9a421acb7b
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-03-23 21:17:53 +0200

    Remove hardcoded urls form navigation

--- students/urls.py
+++ students/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('students.views',
     url(r'^login/$', 'login', name='login'),
     url(r'^accounts/profile/$', 'user_profile', name='user-profile'),
-    url(r'^accounts/edit/$', 'edit_profile', name='edit_profile'),
+    url(r'^accounts/edit/$', 'edit_profile', name='edit-profile'),
     url(r'^logout/$', 'logout', name='logout'),
 )
