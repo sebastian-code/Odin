88f17a04e1f566995aa538ebb043098152584349
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-05-03 23:33:12 +0300

    Fix add_user issue

--- students/models.py
+++ students/models.py
@@ -1,7 +1,27 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from awesome_avatar.fields import AvatarField
 from courses.models import Course
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=UserManager.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        user = self.create_user(email, password=password)
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractUser):
@@ -14,9 +34,10 @@ class User(AbstractUser):
 
     AbstractUser._meta.get_field('email')._unique = True
     AbstractUser.REQUIRED_FIELDS.remove('email')
-    
+    objects = UserManager()
     USERNAME_FIELD = 'email'
-    
+    REQUIRED_FIELDS = []
+
     def __unicode__(self):
         return unicode(self.username)
 
@@ -25,7 +46,7 @@ class CourseAssignment(models.Model):
     user = models.ForeignKey(User)
     course = models.ForeignKey(Course)
     points = models.PositiveIntegerField(default='0')
-    
+
     class Meta:
         unique_together = ('user', 'course')
 
@@ -38,3 +59,5 @@ class UserNote(models.Model):
     assignment = models.ForeignKey(CourseAssignment)
     author = models.ForeignKey(User, null=True, blank=True)
     post_time = models.DateTimeField(auto_now=True)
+
+
