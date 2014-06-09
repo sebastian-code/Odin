e3f14b3b609a441a29b9ff0ce5f53d9bb13667ed
Ivaylo Bachvarov <bachvarof@gmail.com>
2014-06-10 00:02:51 +0300

    Add an HR support

--- students/views.py
+++ students/views.py
@@ -40,7 +40,8 @@ def edit_profile(request):
 @login_required
 def assignment(request, id):
     assignment = get_object_or_404(CourseAssignment, pk=id)
-    is_teacher = request.user.has_perm('student.add_courseassignment')
+    is_teacher = request.user.has_perm('students.edit_usernote')
+    is_hr = request.user.has_perm('students.add_usernote')
 
     if is_teacher:
         notes = UserNote.objects.filter(assignment=id)
