02704a92d7ccd765fead1e2672a03770f31f8fc4
syndg <syndbe@gmail.com>
2014-07-27 21:49:34 +0300

    Fixed some typos in students' templates :zap: Also changed /courses/admin.py's imports to fit the conventions

--- courses/admin.py
+++ courses/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from adminsortable.admin import SortableAdminMixin
+
 from models import Course, Partner
 
-from adminsortable.admin import SortableAdminMixin
 
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
