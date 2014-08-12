f491fa9b5c8c9f77b8ce70107e35569230d57451
syndg <syndbe@gmail.com>
2014-08-12 16:30:10 +0300

    Updated students.validators' validate_linkedin. linkedin.com redirects to www.linkedin.com always.

--- students/validators.py
+++ students/validators.py
@@ -22,7 +22,7 @@ def validate_github(url):
 
 
 def validate_linkedin(url):
-    validate_url(url, '://linkedin.com/', '{} is not a valid Linkedin account URL'.format(url), 'invalid_linkedin_account_url')
+    validate_url(url, '://www.linkedin.com/', '{} is not a valid Linkedin account URL'.format(url), 'invalid_linkedin_account_url')
 
 
 # Form validators
