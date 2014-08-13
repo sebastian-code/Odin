fedefa0fe6f8d067f9d7982eedf58595462972ca
syndg <syndbe@gmail.com>
2014-08-14 00:47:12 +0300

    Refactored the generate_certificates script. Now it should be easier to test and maintain.

--- students/management/commands/generate_certificates.py
+++ students/management/commands/generate_certificates.py
@@ -3,74 +3,32 @@ from django.core.management.base import BaseCommand
 
 from students.models import CourseAssignment, Solution
 from .helpers.classes import TempCertificate, SolutionGithubRepo
-from courses.models import WeeklyCommit, Certificate
-from datetime import datetime
 
 
 def generate_certificate(assignment, solutions):
     visited_repos = []
-    temp_certificate = TempCertificate()
-    is_cheater = False
-    cheated_solutions = []
+    temp_certificate = TempCertificate(assignment)
 
     for solution in solutions:
         github_parameters = solution.get_github_user_and_repo_names()
         if is_new_valid_github_account(github_parameters, visited_repos):
-            solution_github_repo = SolutionGithubRepo(github_parameters['user'], github_parameters['repo_name'])
-            if is_cheating(github_parameters, solution, solution_github_repo):
-                cheated_solutions.append(solution)
-                is_cheater = True
+            solution_github_repo = SolutionGithubRepo(github_parameters['user'], github_parameters['repo_name'], solution)
+            if solution_github_repo.is_cheating():
+                temp_certificate.add_cheated_solution(solution)
                 continue
             api_stats = solution_github_repo.get_stats()
             temp_certificate.update_stats(api_stats)
-            create_db_weekly_commit(solution, solution_github_repo, temp_certificate.weekly_commits)
+            temp_certificate.create_db_weekly_commit(solution, solution_github_repo)
             visited_repos.append(github_parameters)
-
-    if is_cheater:
-        log_cheating(cheated_solutions)
-    else:
-        Certificate.objects.filter(assignment=assignment).delete()
-        print 'Created certificate for <{}>'.format(assignment.user)
-        create_db_certificate(assignment, temp_certificate)
+    temp_certificate.log_or_add_in_db()
 
 
 def is_new_valid_github_account(github_parameters, visited_repos):
-    return 'user' in github_parameters and 'repo_name' in github_parameters and not github_parameters in visited_repos
-
-
-def is_cheating(github_parameters, solution, solution_github_repo):
-    profile_github_account = solution.get_user_github_username()
-    return github_parameters['user'] != profile_github_account and solution_github_repo.api_repo.has_in_collaborators(profile_github_account) == False
-
-
-def log_cheating(cheated_solutions):
-    task = cheated_solutions[0].task
-    user = cheated_solutions[0].user
-    filename = 'Cheater[{}] {} - {}'.format(task.course, user.get_full_name(), user.email)
-    f = open(filename, 'w+')
-    for i, solution in enumerate(cheated_solutions, start=1):
-        f.write('{}) Cheated on task {} - {}. Given solution - {}'.format(i, task.name, task.description, solution.repo) + '\n')
-    f.close()
-
-
-def create_db_certificate(assignment, certificate_object):
-    db_certificate = Certificate.objects.create(
-        assignment=assignment, issues_closed=certificate_object.closed_issues, issues_opened=certificate_object.open_issues, total_commits=certificate_object.get_total_commits())
-    # can't reference a list as a ManyToManyField argument
-    for w in certificate_object.weekly_commits:
-        db_certificate.weekly_commits.add(w)
-
-
-def create_db_weekly_commit(solution, solution_github_repo, weekly_commits):
-    model_startime = solution.task.course.application_until
-    start_time = datetime(year=model_startime.year, month=model_startime.month, day=model_startime.day)
-
-    model_endtime = solution.task.deadline
-    end_time = datetime(year=model_endtime.year, month=model_endtime.month, day=model_endtime.day)
-
-    commits_count = solution_github_repo.get_commits_count(start_time, end_time)
-    weekly_commit = WeeklyCommit.objects.create(commits_count=commits_count)
-    weekly_commits.append(weekly_commit)
+    if 'user' in github_parameters and 'repo_name' in github_parameters:
+        for param in visited_repos:
+            if param['user'] == github_parameters['user'] and param['repo_name'] == github_parameters['repo_name']:
+                return False
+    return True
 
 
 class Command(BaseCommand):
@@ -84,6 +42,5 @@ class Command(BaseCommand):
         assignments = CourseAssignment.objects.filter(course__id=arg_course_id)
         for assignment in assignments:
             solutions = Solution.objects.filter(user=assignment.user, task__course__id=arg_course_id)
-
             if assignment.has_valid_github_account() and solutions:
                 generate_certificate(assignment, solutions)
--- students/management/commands/helpers/classes.py
+++ students/management/commands/helpers/classes.py
@@ -1,14 +1,20 @@
 from django.conf import settings
+from courses.models import WeeklyCommit, Certificate
+
 from github import Github, GithubException
+from datetime import datetime
 
 
 class TempCertificate:
 
-    def __init__(self):
+    def __init__(self, assignment):
+        self.assignment = assignment
         self.weekly_commits = []
         self.open_issues = 0
         self.closed_issues = 0
         self.total_commits = 0
+        self.has_cheated = True
+        self.cheated_solutions = []
 
     def get_total_commits(self):
         return reduce(lambda x, y: x + y, map(lambda x: x.commits_count, self.weekly_commits))
@@ -17,18 +23,62 @@ class TempCertificate:
         self.open_issues += api_stats_dictionary['open_issues']
         self.closed_issues += api_stats_dictionary['closed_issues']
 
+    def add_cheated_solution(self, db_solution):
+        self.cheated_solutions.append(db_solution)
+        self.has_cheated = True
+
+    def create_db_weekly_commit(self, db_solution, solution_github_repo):
+        model_startime = db_solution.task.course.application_until
+        start_time = datetime(year=model_startime.year, month=model_startime.month, day=model_startime.day)
+
+        model_endtime = db_solution.task.deadline
+        end_time = datetime(year=model_endtime.year, month=model_endtime.month, day=model_endtime.day)
+
+        commits_count = solution_github_repo.get_commits_count(start_time, end_time)
+        weekly_commit = WeeklyCommit.objects.create(commits_count=commits_count)
+        self.weekly_commits.append(weekly_commit)
+
+    def log_cheating(self):
+        course_name = self.cheated_solutions[0].task.course
+        user = self.cheated_solutions[0].user
+        filename = 'Cheater[{}] {} - {}'.format(course_name, user, user.email)
+        f = open(filename, 'w+')
+        for i, solution in enumerate(self.cheated_solutions, start=1):
+            f.write('{}) Cheated on task {} - {}. Given solution - {}\n'.format(i, solution.task.name, solution.task.description, solution.repo))
+        f.close()
+
+    def create_db_certificate(self):
+        db_certificate = Certificate.objects.create(assignment=self.assignment, issues_closed=self.closed_issues, issues_opened=self.open_issues, total_commits=self.get_total_commits())
+        # can't reference a list as a ManyToManyField argument
+        for w in self.weekly_commits:
+            db_certificate.weekly_commits.add(w)
+
+    def log_or_add_in_db(self):
+        if self.has_cheated and self.cheated_solutions:
+            self.log_cheating()
+        else:
+            Certificate.objects.filter(assignment=self.assignment).delete()
+            self.create_db_certificate()
+            print 'Created certificate for {}'.format(self.assignment.user)
+
 
 class SolutionGithubRepo:
 
-    def __init__(self, user, repo_name):
+    def __init__(self, user, repo_name, db_solution):
+        self.db_solution = db_solution
         self.api_repo = self.__set_api_repo(user, repo_name)
 
     def __set_api_repo(self, user, repo_name):
         github_client = Github(settings.GITHUB_OATH_TOKEN)
         return github_client.get_user(user).get_repo(repo_name)
 
+    def is_cheating(self):
+        profile_github_account = self.db_solution.get_user_github_username()
+        return self.api_repo.has_in_collaborators(profile_github_account) is False
+
     def get_commits_count(self, start, end):
-        commits = self.api_repo.get_commits(since=start, until=end)
+        author = self.db_solution.get_user_github_username()
+        commits = self.api_repo.get_commits(since=start, until=end, author=author)
         count = 0
         for c in commits:
             count += 1
