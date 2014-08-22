dfb2ecb0229b7f8a94af79cb7619dda59e53eda4
syndg <syndbe@gmail.com>
2014-08-22 19:20:30 +0300

    Refactored and extended tests in forum.tests

--- forum/tests.py
+++ forum/tests.py
@@ -7,7 +7,7 @@ from students.models import User
 from .models import Category, Topic, Comment
 
 
-class CoursesTest(TestCase):
+class ForumTest(TestCase):
 
     def setUp(self):
         self.student_user = User.objects.create_user('ivo_student@gmail.com', '123')
@@ -35,33 +35,38 @@ class CoursesTest(TestCase):
             topic=self.topic,
         )
 
-    def test_show_categories_status(self):
+    def test_show_categories(self):
         self.client = client.Client()
         response = self.client.get(reverse('forum:forum'))
         self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed('categories.html', response)
 
-    def test_show_category_status(self):
+    def test_show_category(self):
         self.client = client.Client()
         response = self.client.get(
             reverse('forum:show-category', kwargs={'category_id': self.category.id}))
         self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed('show_category.html', response)
 
-    def test_show_nonexistent_category_status(self):
+    def test_show_nonexistent_category(self):
         self.client = client.Client()
         response = self.client.get(reverse('forum:show-category', kwargs={'category_id': '234'}))
         self.assertEqual(404, response.status_code)
+        self.assertTemplateNotUsed('show_category.html', response)
 
-    def test_show_topic_status(self):
+    def test_show_topic(self):
         self.client = client.Client()
         response = self.client.get(reverse('forum:show-topic', kwargs={'topic_id': self.topic.id}))
         self.assertEqual(200, response.status_code)
+        self.assertTemplateUsed('show_topic.html', response)
 
-    def test_show_nonexistent_topic_status(self):
+    def test_show_nonexistent_topic(self):
         self.client = client.Client()
         response = self.client.get(reverse('forum:show-topic', kwargs={'topic_id': '234'}))
         self.assertEqual(404, response.status_code)
+        self.assertTemplateNotUsed('show_topic.html', response)
 
-    def test_add_topic_status(self):
+    def test_add_topic(self):
         self.client = client.Client()
         self.client.login(username='ivo_student@gmail.com', password='123')
         before_add = Topic.objects.count()
@@ -73,6 +78,8 @@ class CoursesTest(TestCase):
 
         after_add = Topic.objects.count()
         self.assertEqual(before_add + 1, after_add)
+        self.assertRedirects(response, 'category/{}/'.format(self.category.id))
+        self.assertTemplateUsed('add_topic.html', response)
 
     def test_add_topic_not_logged_redirect(self):
         self.client = client.Client()
@@ -87,6 +94,7 @@ class CoursesTest(TestCase):
 
         self.assertEqual(before_add, after_add)
         self.assertRedirects(response, 'login/?next=/add-topic/{}/'.format(self.category.id))
+        self.assertTemplateNotUsed('add_topic.html', response)
 
     def test_edit_topic(self):
         self.client = client.Client()
@@ -98,6 +106,7 @@ class CoursesTest(TestCase):
 
         edited_topic = Topic.objects.filter(title='My Topic 2')
         self.assertEqual(1, edited_topic.count())
+        self.assertTemplateUsed('edit_topic.html', response)
 
     def test_edit_topic_not_logged(self):
         self.client = client.Client()
@@ -107,8 +116,9 @@ class CoursesTest(TestCase):
         })
 
         self.assertRedirects(response, 'login/?next=/edit-topic/{}/'.format(self.topic.id))
+        self.assertTemplateNotUsed('edit_topic.html', response)
 
-    def test_edit_not_owned(self):
+    def test_edit_topic_not_owned(self):
         self.client = client.Client()
         self.client.login(username='ivo_hr@gmail.com', password='123')
         response = self.client.post(reverse('forum:edit-topic',  kwargs={'topic_id': self.topic.id}), {
@@ -117,6 +127,7 @@ class CoursesTest(TestCase):
         })
 
         self.assertEqual(403, response.status_code)
+        self.assertTemplateNotUsed('edit_topic.html', response)
 
     def test_add_comment(self):
         self.client = client.Client()
@@ -129,6 +140,19 @@ class CoursesTest(TestCase):
 
         after_add = Comment.objects.count()
         self.assertEqual(before_add + 1, after_add)
+        self.assertTemplateUsed('show_topic.html', response)
+
+    def test_add_comment_not_logged(self):
+        self.client = client.Client()
+        before_add = Comment.objects.count()
+        response = self.client.post(reverse('forum:show-topic', kwargs={'topic_id': self.topic.id}), {
+            'text': 'Lqlqlq',
+            'topic': self.topic,
+        })
+
+        after_add = Comment.objects.count()
+        self.assertEqual(before_add, after_add)
+        self.assertTemplateNotUsed('show_topic.html', response)
 
     def test_edit_comment(self):
         self.client = client.Client()
@@ -141,6 +165,7 @@ class CoursesTest(TestCase):
         edited_comment = Comment.objects.filter(text=new_text)
         self.assertRedirects(response, 'topic/{}/'.format(edited_comment.first().id))
         self.assertEqual(1, edited_comment.count())
+        self.assertTemplateUsed('edit_comment.html', response)
 
     def test_edit_comment_not_owned(self):
         self.client = client.Client()
@@ -151,19 +176,9 @@ class CoursesTest(TestCase):
         })
 
         self.assertEqual(403, response.status_code)
+        self.assertTemplateNotUsed('edit_comment.html', response)
 
-    def test_add_comment_not_logged(self):
-        self.client = client.Client()
-        before_add = Comment.objects.count()
-        response = self.client.post(reverse('forum:show-topic', kwargs={'topic_id': self.topic.id}), {
-            'text': 'Lqlqlq',
-            'topic': self.topic,
-        })
-
-        after_add = Comment.objects.count()
-        self.assertEqual(before_add, after_add)
-
-# Testing subscribitons
+    # Testing subscriptions
     def test_unsubscribing(self):
         self.client = client.Client()
 
@@ -204,7 +219,7 @@ class CoursesTest(TestCase):
 
         self.assertEqual(302, response.status_code)
 
-    def test_subscribing_afther_new_topic(self):
+    def test_subscribing_after_new_topic(self):
         self.client = client.Client()
         self.client.login(
             username=self.student_user.email,
@@ -214,16 +229,16 @@ class CoursesTest(TestCase):
         response = self.client.post(
             reverse('forum:add-topic',  kwargs={'category_id': self.category.id}),
             {
-                'title': 'test subscriging afther new topic',
+                'title': 'test subscriging after new topic',
                 'text': 'Lqlqlq',
                 'category': self.category,
             }
         )
 
-        new_topic = Topic.objects.filter(title="test subscriging afther new topic").first()
+        new_topic = Topic.objects.filter(title="test subscriging after new topic").first()
         self.assertTrue(new_topic in self.student_user.subscribed_topics.all())
 
-    def test_subscribing_afther_new_topic(self):
+    def test_subscribing_after_new_topic(self):
         self.client = client.Client()
         self.client.login(
             username=self.student_user.email,
@@ -233,16 +248,17 @@ class CoursesTest(TestCase):
         response = self.client.post(
             reverse('forum:add-topic',  kwargs={'category_id': self.category.id}),
             {
-                'title': 'test subscriging afther new topic',
+                'title': 'test subscriging after new topic',
                 'text': 'Lqlqlq',
                 'category': self.category,
             }
         )
 
-        new_topic = Topic.objects.filter(title="test subscriging afther new topic").first()
+        new_topic = Topic.objects.filter(title="test subscriging after new topic").first()
         self.assertTrue(new_topic in self.student_user.subscribed_topics.all())
+        self.assertTemplateUsed('add_topic.html', response)
 
-    def test_subscribing_afther_new_comment(self):
+    def test_subscribing_after_new_comment(self):
         self.client = client.Client()
         self.client.login(
             username=self.student_user.email,
@@ -261,8 +277,9 @@ class CoursesTest(TestCase):
         })
 
         self.assertTrue(empty_topic in self.student_user.subscribed_topics.all())
+        self.assertTemplateUsed('show_topic.html', response)
 
-    def test_subscribing_afther_unsubscribing(self):
+    def test_subscribing_after_unsubscribing(self):
         self.client = client.Client()
         self.client.login(
             username=self.student_user.email,
@@ -290,6 +307,7 @@ class CoursesTest(TestCase):
         })
 
         self.assertTrue(empty_topic not in self.student_user.subscribed_topics.all())
+        self.assertTemplateUsed('show_topic.html', response)
 
     def test_sending_emails(self):
         self.client = client.Client()
