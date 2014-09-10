e8a4acd3ee5330b0fec9218584beb57a62635efe
syndg <syndbe@gmail.com>
2014-09-10 18:58:33 +0300

    Added initial views

--- statistics/views.py
+++ statistics/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def show_all_stats(request):
+    pass
+
+
+def show_partners_stats(request):
+    pass
+
+
+def show_companies_stats(request):
+    pass
+
+
+def show_partner_stats(request, partner_id):
+    pass
+
+
+def show_company_stats(request, company_id):
+    pass
+
+
+def show_assignments_stats(request):
+    pass
+
+
+def show_courses_stats(request):
+    pass
+
+
+def show_course_stats(request, course_id):
+    pass
