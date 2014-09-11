1a7edf1235b523774c0e51bbe6bee2adc14ed615
syndg <syndbe@gmail.com>
2014-09-11 15:47:55 +0300

    Added money_spent field to Partner model.

--- courses/models.py
+++ courses/models.py
@@ -33,6 +33,7 @@ class Partner(models.Model):
     facebook = models.URLField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
     logo = models.ImageField(upload_to="partner_logoes", null=True, blank=True)
+    money_spent = models.PositiveIntegerField(default=0, blank=False, null=False)
     name = models.CharField(max_length=128)
     ordering = models.PositiveSmallIntegerField(default=0, blank=False, null=False)
     twitter = models.URLField(null=True, blank=True)
