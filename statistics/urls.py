446b0a4de37d0dbfd61d4043982b776f78ddef32
syndg <syndbe@gmail.com>
2014-09-14 13:59:02 +0300

    Added basic partner statistics

--- statistics/urls.py
+++ statistics/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, url
 
 
+
 urlpatterns = patterns('statistics.views',
-    url(r'^all/$', 'show_all_stats', name='show_all_stats'),
-    url(r'^partners/$', 'show_partners_stats', name='show_partner_stats'),
+    url(r'^dashboard/$', 'dashboard', name='dashboard'),
+    url(r'^partners/$', 'show_partners_stats', name='show_partners_stats'),
     url(r'^partners/(?P<partner_id>[0-9]+)/$', 'show_partner_stats', name='show_partner_stats'),
     url(r'^companies/$', 'show_companies_stats', name='show_companies_stats'),
     url(r'^companies/(?P<company_id>[0-9]+)/$', 'show_company_stats', name='show_company_stats'),
--- statistics/views.py
+++ statistics/views.py
@@ -1,17 +1,27 @@
-from django.shortcuts import render
+from django.core.urlresolvers import reverse
 from django.contrib.admin.views.decorators import staff_member_required
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
 
-from courses.models import Partner
+from courses.models import Course, Partner
 from students.models import CourseAssignment, StudentStartedWorkingAt
+from .forms import SelectCompanyForm, SelectPartnerForm
 
 
+# TODO: Needs more research.
 @staff_member_required
-def show_all_stats(request):
-    pass
+def dashboard(request):
+    select_company_form = SelectCompanyForm()
+    select_partner_form = SelectPartnerForm()
+    if select_company_form.is_valid():
+        partner_id = Partner.objects.get(name='')
+        return HttpResponseRedirect(reverse('statistics:show_partner_stats', args=[]))
+    return render(request, 'dashboard.html', locals())
 
 
 @staff_member_required
 def show_partners_stats(request):
+    partners = Partner.objects.filter(is_active=True)
     return render(request, 'show_stats.html', locals())
 
 
@@ -19,13 +29,16 @@ def show_partners_stats(request):
 def show_partner_stats(request, partner_id):
     partner = Partner.objects.get(pk=partner_id)
     total_assignments = CourseAssignment.objects.filter(course__partner=partner).count()
-    assignments = StudentStartedWorkingAt.objects.filter(partner=partner)
-    cost_per_recruitment = partner.money_spent / assignments.count() if assignments.count() > 0 else 0
+    started_working_ats = StudentStartedWorkingAt.objects.filter(partner=partner).select_related('assignment')
+    started_working_ats_count = started_working_ats.count()
+    cost_per_recruitment = partner.money_spent / started_working_ats_count if started_working_ats.count() > 0 else 0
+    hired_percent = total_assignments / started_working_ats
     return render(request, 'show_partner_company_stats.html', locals())
 
 
 @staff_member_required
 def show_companies_stats(request):
+    companies = Partner.objects.filter(is_active=False)
     return render(request, 'show_stats.html', locals())
 
 
@@ -37,9 +50,11 @@ def show_assignments_stats(request):
 
 @staff_member_required
 def show_courses_stats(request):
-    pass
+    courses = Course.objects.all()
+    return render(request)
 
 
 @staff_member_required
 def show_course_stats(request, course_id):
-    pass
+    course = Course.objects.get(pk=course_id)
+    return render(request)
