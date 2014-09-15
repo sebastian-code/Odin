84c3117c717122b80c6a11fb42a238e7eb0df6b5
syndg <syndbe@gmail.com>
2014-09-15 12:42:12 +0300

    Added division_by_zero test for statistics

--- statistics/tests_helpers.py
+++ statistics/tests_helpers.py
@@ -6,3 +6,6 @@ from helpers import division_or_zero
 class HelpersTest(TestCase):
     def test_division(self):
         self.assertAlmostEqual(1.66666666667, division_or_zero(5, 3), places=7)
+
+    def test_division_by_zero(self):
+        self.assertEqual(0, division_or_zero(5, 0))
