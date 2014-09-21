10eec67ab26cdd230664102f9584d429cd94398f
syndg <syndbe@gmail.com>
2014-09-21 21:17:30 +0300

    Added question_list in Poll.admin

--- polls/admin.py
+++ polls/admin.py
@@ -3,7 +3,11 @@ from django.contrib import admin
 from models import Poll, Question, Answer, Choice
 
 
-admin.site.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ['title', 'get_question_list']
+    list_filter = ('is_active',)
+
+admin.site.register(Poll, PollAdmin)
 admin.site.register(Question)
 admin.site.register(Answer)
 admin.site.register(Choice)
