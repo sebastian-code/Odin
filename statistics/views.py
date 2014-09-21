a860e801e109c3ceafed9aebd0bf0f538e001eb5
syndg <syndbe@gmail.com>
2014-09-22 00:27:38 +0300

    Added statistics show_courses_stats. Also major fixed in other statistics.views

--- statistics/views.py
+++ statistics/views.py
@@ -1,4 +1,3 @@
-from django.core.urlresolvers import reverse
 from django.contrib.admin.views.decorators import staff_member_required
 from django.shortcuts import render, get_object_or_404
 
@@ -27,6 +26,7 @@ def show_partners_stats(request):
         average_cost_per_recruitment += partner_cost_per_recruitment
         total_started_working_ats += partner_started_working_ats
         total_money_spent += partner.money_spent
+    average_cost_per_recruitment = division_or_zero(average_cost_per_recruitment, partners.count())
     total_assignments = CourseAssignment.objects.filter(course__partner__in=partners).count()
     total_hired_percent = division_or_zero(total_started_working_ats, total_assignments) * 100
     return render(request, 'show_partners_stats.html', locals())
@@ -55,6 +55,7 @@ def show_companies_stats(request):
         average_cost_per_recruitment += company_cost_per_recruitment
         total_started_working_ats += company_started_working_ats
         total_money_spent += company.money_spent
+    average_cost_per_recruitment = division_or_zero(average_cost_per_recruitment, companies.count())
     total_assignments = CourseAssignment.objects.filter(course__partner__in=companies).count()
     total_hired_percent = division_or_zero(total_started_working_ats, total_assignments) * 100
     return render(request, 'show_companies_stats.html', locals())
@@ -70,8 +71,28 @@ def show_assignments_stats(request):
 # TODO: think of an use
 @staff_member_required
 def show_courses_stats(request):
-    courses = Course.objects.all()
-    return render(request)
+    courses = Course.objects.filter(is_free=True)
+    partners = set()
+    for course in courses:
+        for partner in course.partner.all():
+            partners.add(partner)
+
+    total_courses_funds = 0
+    average_cost_per_recruitment = 0
+
+    for partner in partners:
+        partner_started_working_ats = StudentStartedWorkingAt.objects.filter(partner=partner).count()
+        partner_cost_per_recruitment = division_or_zero(partner.money_spent, partner_started_working_ats)
+        average_cost_per_recruitment += partner_cost_per_recruitment
+        partner_course_funds = division_or_zero(partner.money_spent, Course.objects.filter(partner=partner).count())
+        total_courses_funds += partner_course_funds
+
+    average_cost_per_recruitment = division_or_zero(average_cost_per_recruitment, len(partners))
+    total_assignments = CourseAssignment.objects.filter(course__in=courses).count()
+    total_started_working_ats = StudentStartedWorkingAt.objects.all()
+    total_started_working_ats_count = total_started_working_ats.count()
+    hired_percent = division_or_zero(total_started_working_ats_count, total_assignments) * 100
+    return render(request, 'show_courses_stats.html', locals())
 
 
 @staff_member_required
@@ -79,11 +100,16 @@ def show_course_stats(request, course_id):
     course = get_object_or_404(Course, pk=course_id)
     partners = course.partner.all()
     total_course_funds = 0
+    average_cost_per_recruitment = 0
     for partner in partners:
-        partner_course_funds = partner.money_spent / Course.objects.filter(partner=partner).count()
+        partner_started_working_ats = StudentStartedWorkingAt.objects.filter(partner=partner).count()
+        partner_cost_per_recruitment = division_or_zero(partner.money_spent, partner_started_working_ats)
+        average_cost_per_recruitment += partner_cost_per_recruitment
+        partner_course_funds = division_or_zero(partner.money_spent, Course.objects.filter(partner=partner).count())
         total_course_funds += partner_course_funds
     total_assignments = CourseAssignment.objects.filter(course=course)
     started_working_ats = StudentStartedWorkingAt.objects.filter(assignment__in=total_assignments).select_related('assignment')
     started_working_ats_count = started_working_ats.count()
+    average_cost_per_recruitment = division_or_zero(average_cost_per_recruitment, partners.count())
     hired_percent = division_or_zero(started_working_ats_count, total_assignments.count()) * 100
     return render(request, 'show_course_stats.html', locals())
