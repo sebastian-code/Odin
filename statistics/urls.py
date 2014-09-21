ff0de3d49bc12d0fd9037d30947e5507831b839b
syndg <syndbe@gmail.com>
2014-09-21 23:09:38 +0300

    Fixed courses stats URL being inconsistent. Also commented assignments stats URL since it's unclear what it does for now.

--- statistics/urls.py
+++ statistics/urls.py
@@ -10,9 +10,8 @@ urlpatterns = patterns('statistics.views',
 
     url(r'^companies/all/$', 'show_companies_stats', name='show_companies_stats'),
     # url(r'^companies/(?P<company_id>[0-9]+)/$', 'show_company_stats', name='show_company_stats'),
+    # url(r'^assignments/$', 'show_assignments_stats', name='show_assignments_stats'),
 
-    url(r'^assignments/$', 'show_assignments_stats', name='show_assignments_stats'),
-
-    url(r'^courses/$', 'show_courses_stats', name='show_courses_stats'),
+    url(r'^courses/all/$', 'show_courses_stats', name='show_courses_stats'),
     url(r'^courses/(?P<course_id>[0-9]+)/$', 'show_course_stats', name='show_course_stats'),
 )
