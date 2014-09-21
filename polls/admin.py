acce59f65ba849dd1bd1bf59e98d53cbb388fea4
syndg <syndbe@gmail.com>
2014-09-21 22:23:20 +0300

    Added AnswerAdmin

--- polls/admin.py
+++ polls/admin.py
@@ -8,6 +8,18 @@ class PollAdmin(admin.ModelAdmin):
     list_filter = ('is_active',)
 
 admin.site.register(Poll, PollAdmin)
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ['user_full_name', 'user_email', 'choice']
+    list_filter = ('user',)
+
+    def user_full_name(self, instance):
+        return unicode(instance.user)
+
+    def user_email(self, instance):
+        return instance.user.email
+
+admin.site.register(Answer, AnswerAdmin)
 admin.site.register(Question)
-admin.site.register(Answer)
 admin.site.register(Choice)
