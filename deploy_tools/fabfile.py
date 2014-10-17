da67145f4b67b0a1242434317b3c6ddcc34090b0
syndg <syndbe@gmail.com>
2014-10-17 16:03:12 +0300

    Updated fabfile. Fixed WET code and focused on SRP functions. Most importantly, removed global vars.

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,108 +1,121 @@
 from ConfigParser import ConfigParser
-import random
 import os
+import random
 
 from fabric.api import env, local, run, cd, sudo
-from fabric.operations import prompt
+from fabric.context_managers import settings
 from fabric.contrib.files import append, exists, upload_template
 
 
-REPO_URL = 'https://github.com/HackBulgaria/Odin.git'
-env.host = prompt('Your domain:')
-site_folder = '/home/{}/sites/{}'.format(env.user, env.host)
-source_folder = site_folder + '/source'
+def __generate_dict_from_ini_file_section(section):
+    config_parser = ConfigParser()
+    config_parser.read('fabfile_settings.ini')
+
+    ## Alternative with different API calls. Not sure if I should delete it yet.
+    # dictionary = {}
+    # for option in config_parser.options(section):
+    #     dictionary[option.upper()] = config_parser.get(section, option)
+    return {option.upper(): value for option, value in config_parser.items(section)}
 
 
 def provision():
-    site_folder = '/home/{}/sites/{}'.format(env.user, env.host)
-    _create_directory_structure_if_neccessary()
-    _install_requirements_if_neccessary()
-    _create_db_cluster_database_and_user_with_privileges()
+    app_settings = __generate_dict_from_ini_file_section('APP')
+    site_folder = '/home/{}/sites/{}'.format(env.user, env.host or app_settings['HOST'])
+    linux_packages = app_settings['LINUX_PACKAGES']
+    python_packages = app_settings['PYTHON_PACKAGES']
+    db_settings = __generate_dict_from_ini_file_section('DB')
+
+    _create_directory_structure_if_neccessary(site_folder)
+    _install_requirements_if_neccessary(linux_packages, python_packages)
+    _create_db_cluster_database_and_user_with_privileges(db_settings)
 
 
-def _install_requirements_if_neccessary():
-    sudo('apt-get update')
-    sudo('apt-get install -y git postgresql postgresql-contrib nginx python3-pip python-pip')
-    sudo('pip3 install virtualenv gunicorn jinja2')
-
-
-def _create_db_cluster_database_and_user_with_privileges():
-    sudo('su - postgres')
-    run('pg_createcluster 9.3 main --start')
-    run('createdb odin_db')
-    run('createuser -s -l -P odin_role')
-    run('psql -c \'GRANT ALL PRIVELEGES ON DATABASE odin_db TO odin_role;\'')
-
-
-def deploy():
-    _create_directory_structure_if_neccessary()
-    _get_latest_source()
-    _set_settings()
-    _create_or_update_virtualenv()
-    _update_static_files()
-    _initialize_database()
-
-
-def _initialize_database():
-    with cd(source_folder):
-        run('..virtualenv/bin/python manage.py syncdb --all --noinput')
-        run('..virtualenv/bin/python manage.py migrate --fake --noinput')
-
-
-def update():
-    _get_latest_source()
-    _update_static_files()
-    _update_database()
-
-
-def _create_directory_structure_if_neccessary():
+def _create_directory_structure_if_neccessary(site_folder):
     subfolders_to_create = ('database', 'source', 'static', 'virtualenv')
     for subfolder in subfolders_to_create:
         run('mkdir -p {}/{}'.format(site_folder, subfolder))
 
 
-def _get_latest_source():
+def _install_requirements_if_neccessary(linux_packages, python_packages):
+    sudo('apt-get update')
+    sudo('apt-get install {}'.format(linux_packages))
+    sudo('pip3 install {}'.format(python_packages))
+
+
+def _create_db_cluster_database_and_user_with_privileges(db_settings):
+    with settings(warn_only=True, sudo_user=db_settings['SUDO_USER']):
+        sudo('pg_createcluster {} main --start'.format(db_settings['VERSION']))
+        sudo('createdb {}'.format(db_settings['NAME']))
+        sudo('createuser {} {}'.format(db_settings['PRIVILEGES'], db_settings['USERNAME']))
+        sudo('psql -c \'GRANT ALL PRIVILEGES ON DATABASE {} TO {};\''.format(db_settings['NAME'],
+                                                                             db_settings['USERNAME']))
+
+
+def deploy():
+    app_settings = __generate_dict_from_ini_file_section('APP')
+    site_folder = '/home/{}/sites/{}'.format(env.user, env.host or app_settings['HOST'])
+    source_folder = site_folder + '/source'
+
+    _create_directory_structure_if_neccessary(site_folder)
+    _get_latest_source(source_folder, app_settings)
+    _set_settings(source_folder)
+    _create_or_update_virtualenv(source_folder, app_settings)
+    with cd(source_folder):
+        _initialize_database()
+        _update_static_files()
+
+
+def _get_latest_source(source_folder, app_settings):
     if exists(source_folder + '/.git'):
         with cd(source_folder):
             run('git fetch')
     else:
-        run('git clone {} {}'.format(REPO_URL, source_folder))
+        run('git clone {} {}'.format(app_settings['REPO_URL'], source_folder))
     current_commit = local('git log -n 1 --format=%H', capture=True)
     with cd(source_folder):
         run('git reset --hard {}'.format(current_commit))
 
 
-def _set_settings():
-    template_path = source_folder + '/deploy_tools/local_settings_template.jinja'
+def _initialize_database():
+    run('../virtualenv/bin/python manage.py syncdb --all --noinput')
+    run('../virtualenv/bin/python manage.py migrate --fake --noinput')
+
+
+def update():
+    app_settings = __generate_dict_from_ini_file_section('APP')
+    site_folder = '/home/{}/sites/{}'.format(env.user, env.host or app_settings['HOST'])
+    source_folder = site_folder + '/source'
+
+    _get_latest_source(source_folder, app_settings)
+    _update_static_files()
+    _update_database()
+
+
+def _set_settings(source_folder):
+    template_path = 'local_settings_template.jinja'
     local_settings_path = source_folder + '/odin/local_settings.py'
-    settings_path = source_folder + '/odin/settings.py'
     settings_context = _create_settings_context()
 
     upload_template(filename=template_path, destination=local_settings_path,
                     use_jinja=True, context=settings_context)
+    run('cat {}'.format(local_settings_path))
     BASE_DIR = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
     STATIC_ROOT = os.path.join(BASE_DIR, '../static')
-    append(settings_path, 'BASE_DIR = {}'.format(BASE_DIR))
-    append(settings_path, 'STATIC_ROOT {}'.format(STATIC_ROOT))
+
+    settings_path = source_folder + '/odin/settings.py'
+    append(settings_path, 'BASE_DIR = \'{}\''.format(BASE_DIR))
+    append(settings_path, 'STATIC_ROOT = \'{}\''.format(STATIC_ROOT))
 
 
 def _create_settings_context():
-    config_parser = ConfigParser()
-    config_parser.read('fabfile_settings.ini')
+    context = __generate_dict_from_ini_file_section('DJANGO')
 
-    container_ip = 'ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\''
-
-    SECRET_KEY = config_parser.get('Odin', 'SECRET_KEY') or _generate_secret_key
-    ENGINE = config_parser.get('Odin', 'ENGINE')
-    NAME = config_parser.get('Odin', 'NAME')
-    USER = config_parser.get('Odin', 'USER')
-    PASSWORD = config_parser.get('Odin', 'PASSWORD')
-    HOST = config_parser.get('Odin', 'HOST')
-    PORT = config_parser.get('Odin', 'PORT')
-    ALLOWED_HOSTS = config_parser.get('Odin', 'ALLOWED_HOSTS') or [container_ip, env.host]
-    DEBUG = config_parser.get('Odin', 'DEBUG')
-    GITHUB_OATH_TOKEN = config_parser.get('Odin', 'GITHUB_OATH_TOKEN')
-    return locals()
+    if not context['ALLOWED_HOSTS']:
+        container_ip = run('ifconfig eth0 | grep \'inet addr:\' | cut -d: -f2 | awk \'{ print $1}\'')
+        context['ALLOWED_HOSTS'] = [container_ip, ]
+    if not context['SECRET_KEY']:
+        context['SECRET_KEY'] = _generate_secret_key()
+    return context
 
 
 def _generate_secret_key():
@@ -110,18 +123,16 @@ def _generate_secret_key():
     return ''.join(random.SystemRandom().choice(chars) for _ in range(50))
 
 
-def _create_or_update_virtualenv():
+def _create_or_update_virtualenv(source_folder, app_settings):
     virtualenv_folder = source_folder + '/../virtualenv'
-    if not exists(virtualenv_folder, '/bin/pip'):
-        run('virtualenv --python=python2.7 {}'.format(virtualenv_folder))
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run('virtualenv --python={} {}'.format(app_settings['PYTHON_VERSION'], virtualenv_folder))
     run('{}/bin/pip install -r {}/requirements/stable.pip'.format(virtualenv_folder, source_folder))
 
 
 def _update_static_files():
-    with cd(source_folder):
-        run('../virtualenv/bin/python manage.py collectstatic --noinput')
+    run('../virtualenv/bin/python manage.py collectstatic --noinput')
 
 
 def _update_database():
-    with cd(source_folder):
-        run('../virtualenv/bin/python manage.py migrate --noinput')
+    run('../virtualenv/bin/python manage.py migrate --noinput')
