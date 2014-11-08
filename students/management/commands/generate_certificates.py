2da06c78770d0dbaa2bd7cb72e8b0d918b89a268
syndg <syndbe@gmail.com>
2014-11-08 02:44:00 +0200

    Restructured generate_certificates.py

--- students/management/commands/generate_certificates.py
+++ students/management/commands/generate_certificates.py
@@ -4,6 +4,21 @@ from students.models import CourseAssignment, Solution
 from .helpers.classes import TempCertificate, GithubSolution
 
 
+class Command(BaseCommand):
+    args = '<course_id>'
+    help = '''
+        Generates certificates for every student from <course_id>.
+    '''
+
+    def handle(self, *args, **options):
+        arg_course_id = args[0]
+        assignments = CourseAssignment.objects.filter(course__id=arg_course_id)
+        for assignment in assignments:
+            solutions = Solution.objects.filter(user=assignment.user, task__course__id=arg_course_id)
+            if assignment.has_valid_github_account() and solutions:
+                generate_certificate(assignment, solutions)
+
+
 def generate_certificate(assignment, solutions):
     temp_certificate = TempCertificate(assignment)
     visited_repos = []
@@ -15,9 +30,9 @@ def generate_certificate(assignment, solutions):
             solution_github_repo = GithubSolution(github_parameters['user_name'], github_parameters['repo_name'], solution)
             if solution_github_repo.is_invalid_repo():
                 return
-            elif solution_github_repo.is_cheating():
-                temp_certificate.add_cheated_solution(solution)
-                continue
+            # elif solution_github_repo.is_cheating():
+            #     temp_certificate.add_cheated_solution(solution)
+            #     continue
             solution_github_repo.update_commits_count(temp_certificate.start_time, temp_certificate.end_time)
             if not solution_github_repo.is_fork() and solution_github_repo.commits > 0:
                 api_stats = solution_github_repo.get_stats()
@@ -33,18 +48,3 @@ def is_new_valid_github_account(github_parameters, visited_repos):
             if param['user_name'] == github_parameters['user_name'] and param['repo_name'] == github_parameters['repo_name']:
                 return False
     return True
-
-
-class Command(BaseCommand):
-    args = '<course_id>'
-    help = '''
-        Generates certificates for every student from <course_id>.
-    '''
-
-    def handle(self, *args, **options):
-        arg_course_id = args[0]
-        assignments = CourseAssignment.objects.filter(course__id=arg_course_id)
-        for assignment in assignments:
-            solutions = Solution.objects.filter(user=assignment.user, task__course__id=arg_course_id)
-            if assignment.has_valid_github_account() and solutions:
-                generate_certificate(assignment, solutions)
