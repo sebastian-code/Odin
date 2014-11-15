589fb1b9bb741583def55a81d65c236e1cbf95ff
syndg <syndbe@gmail.com>
2014-11-16 00:29:52 +0200

    Change show_course_students URL to be more consistent.

--- courses/urls.py
+++ courses/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('courses.views',
     url(r'^course/(?P<course_url>\S+)/$', 'show_course', name='show_course'),
     url(r'^courses/$', 'show_all_courses', name='show_all_courses'),
     url(r'^course/(?P<course_id>[0-9]+)/solutions$', 'show_submitted_solutions', name='show_submitted_solutions'),
-    url(r'^course_students/(?P<course_id>[0-9]+)/$', 'show_course_students', name='show_course_students'),
+    url(r'^course/(?P<course_id>[0-9]+)/students$', 'show_course_students', name='show_course_students'),
 
     url(r'^partners/$', 'show_all_partners', name='show_all_partners'),
     url(r'^certificate/(?P<assignment_id>[0-9]+)/$', 'show_certificate', name='show_certificate'),
