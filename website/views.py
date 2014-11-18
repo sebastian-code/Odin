f5ef43cf5665666f624d13059535a4cc7fec9a31
syndg <syndbe@gmail.com>
2014-11-18 18:50:51 +0200

    Added custom 500 and 404 error pages

--- website/views.py
+++ website/views.py
@@ -6,3 +6,11 @@ from courses.models import Course
 def index(request):
     courses = Course.objects.filter(show_on_index=True)
     return render(request, 'index.html', locals())
+
+
+def page_not_found(request):
+    return render(request, '404.html')
+
+
+def server_error(request):
+    return render(request, '500.html')
