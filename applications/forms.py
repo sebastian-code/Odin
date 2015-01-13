47c28cf05af3a451cb4ba1f2161d7be8ed7814e0
syndbg <syndbe@gmail.com>
2015-01-13 16:04:16 +0200

    Updated ApplicationForm in favor of cleaner Django code

--- applications/forms.py
+++ applications/forms.py
@@ -1,32 +1,54 @@
 from django import forms
 from django.utils import timezone
 
+from applications.models import Application
 from students.models import User
 from courses.models import Course
 
 
-class ApplicationForm(forms.Form):
+EMAIL_DUPLICATE_ERROR = 'Този email вече е регистриран.'
+NAMES_ERROR = 'Моля въведете поне две имена.'
+
+
+class ApplicationForm(forms.ModelForm):
     course = forms.ModelChoiceField(
         label='За кой курс кандидатстваш',
         queryset=Course.objects.filter(application_until__gt=timezone.now())
     )
-    name = forms.CharField(label='Как се казваш', max_length=100)
+    name = forms.CharField(label='Как се казваш', widget=forms.TextInput(attrs={'placeholder': 'Две имена'}), max_length=100)
     email = forms.EmailField(label='Email')
     skype = forms.CharField(label='Skype', max_length=100)
     phone = forms.CharField(label='Телефон', max_length=100)
 
     def is_valid(self):
-        valid = super(ApplicationForm, self).is_valid()
-
+        valid = super().is_valid()
+        print('Form', valid)
         if not valid:
             return valid
 
         if User.is_existing(self.cleaned_data['email']):
-            self._errors['email_registered'] = 'Този email вече е регистриран.'
+            self._errors['email_registered'] = EMAIL_DUPLICATE_ERROR
             return False
 
-        if len(self.cleaned_data['name'].split()) != 2:
-            self._errors['name_not_full'] = 'Моля въведете поне две имена.'
+        if len(self.cleaned_data['name'].split()) < 2:
+            self._errors['name_not_full'] = NAMES_ERROR
             return False
-
         return True
+
+    def save(self):
+        email = self.cleaned_data['email']
+        name = self.cleaned_data['name']
+        password = User.generate_password()
+
+        new_user = User.objects.create_user(email, password)
+        new_user.set_full_name(name)
+        new_user.save()
+
+        self.instance.student = new_user
+        return super().save()
+
+    class Meta:
+        model = Application
+        fields = (
+            'course',
+        )
--- applications/views.py
+++ applications/views.py
@@ -1,26 +1,13 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponseRedirect
-
-from students.models import User
 
 from .forms import ApplicationForm
-from .helper import register_user
-from .models import Application
 
 
 def apply(request):
     if request.method == 'POST':
         form = ApplicationForm(request.POST)
         if form.is_valid():
-            new_user = register_user(form.cleaned_data)
-            wanted_course = form.cleaned_data['course']
-
-            application = Application(
-                student=new_user,
-                course=wanted_course
-            )
-            application.save()
-
+            form.save()
             return redirect('applications:thanks')
 
     else:
