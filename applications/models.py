d8a1c882aae828cf5365c98cc1547564594db3c7
syndbg <syndbe@gmail.com>
2015-01-13 15:09:36 +0200

    Added ApplicationTask and ApplicationSolution

--- applications/models.py
+++ applications/models.py
@@ -1,13 +1,21 @@
 from django.db import models
 
-from students.models import User
-from courses.models import Course
-
 
 class Application(models.Model):
-    student = models.ForeignKey(User)
-    course = models.ForeignKey(Course)
+    student = models.ForeignKey('students.User')
+    course = models.ForeignKey('courses.Course')
     date = models.DateField(auto_now=True)
 
     class Meta:
         unique_together = ('student', 'course')
+
+
+class ApplicationTask(models.Model):
+    course = models.ForeignKey('courses.Course')
+    title = models.CharField(blank=False, max_length=255)
+    url = models.URLField()
+
+
+class ApplicationSolution(models.Model):
+    task = models.ForeignKey(ApplicationTask)
+    user = models.ForeignKey('students.User')
