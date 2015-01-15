db33b4112c6706aa17c4848383e931a3a8246449
syndbg <syndbe@gmail.com>
2015-01-15 15:20:27 +0200

    Updated applications submit solutions URL

--- applications/urls.py
+++ applications/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('applications.views',
-    url(r'^$', 'apply', name='apply'),
+    url(r'^apply/$', 'apply', name='apply'),
     url(r'^thanks/$', 'thanks', name='thanks'),
-    url(r'^add-solution/$', 'add_solution', name='add_solution'),
-    url(r'^solutions/(?P<course_url>\S+)/$', 'solutions', name='solutions'),
+    url(r'^applications/add-solution/$', 'add_solution', name='add_solution'),
+    url(r'^applications/(?P<course_url>\S+)/$', 'show_submitted_applications', name='show_submitted_applications'),
+    url(r'^applications/(?P<course_url>\S+)/solutions/$', 'solutions', name='solutions'),
 )
