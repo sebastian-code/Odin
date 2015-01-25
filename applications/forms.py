6fea195c60895304b6e49c4f84ad65f9584bbfc8
syndbg <syndbe@gmail.com>
2015-01-26 01:41:14 +0200

    Added ExistingUserApplicationForm

--- applications/forms.py
+++ applications/forms.py
@@ -3,13 +3,14 @@ from django.template.loader import render_to_string
 from django.utils import timezone
 
 from applications.models import Application, ApplicationSolution
-from students.models import User
 from courses.models import Course
+from students.models import CourseAssignment
+from students.models import User
 from students.validators import validate_github, validate_linkedin
 
 
 EMAIL_DUPLICATE_ERROR = 'Този email вече е регистриран.'
-NAMES_ERROR = 'Моля въведете поне две имена.'
+NAMES_ERROR = 'Моля въведете две имена.'
 
 
 class ApplicationForm(forms.ModelForm):
@@ -21,22 +22,20 @@ class ApplicationForm(forms.ModelForm):
     email = forms.EmailField(label='Email')
     skype = forms.CharField(label='Skype', max_length=100)
     phone = forms.CharField(label='Телефон', max_length=100)
-    github_account = forms.CharField(label='Github', widget=forms.TextInput(attrs={'placeholder': 'https://github.com/HackBulgaria'}), max_length=100, validators=[validate_github])
-    linkedin_acount = forms.CharField(label='Linkedin', widget=forms.TextInput(attrs={'placeholder': 'https://www.linkedin.com/'}), max_length=100, validators=[validate_linkedin])
+    github_account = forms.CharField(label='Github', widget=forms.TextInput(attrs={'placeholder': 'https://github.com/HackBulgaria'}), max_length=100, validators=[validate_github], required=False)
+    linkedin_acount = forms.CharField(label='Linkedin', widget=forms.TextInput(attrs={'placeholder': 'https://www.linkedin.com/'}), max_length=100, validators=[validate_linkedin], required=False)
 
-    def is_valid(self):
-        valid = super().is_valid()
-        if not valid:
-            return valid
+    def clean(self):
+        cleaned_data = super(ApplicationForm, self).clean()
+        email = cleaned_data.get('email')
+        name = cleaned_data.get('name', '')
 
-        if User.is_existing(self.cleaned_data['email']):
+        if User.is_existing(email):
             self.add_error('email', EMAIL_DUPLICATE_ERROR)
-            return False
 
-        if len(self.cleaned_data['name'].split(' ')) < 2:
+        if len(name.split(' ')) < 2:
             self.add_error('name', NAMES_ERROR)
-            return False
-        return True
+        return cleaned_data
 
     def save(self):
         course = self.cleaned_data['course']
@@ -69,6 +68,29 @@ class ApplicationForm(forms.ModelForm):
         model = Application
         fields = (
             'course',
+        )
+
+
+class ExistingUserApplicationForm(forms.ModelForm):
+
+    course = forms.ModelChoiceField(
+        label='За кой курс кандидатстваш',
+        queryset=Course.objects.filter(application_until__gte=timezone.now())
+    )
+
+    def __init__(self, *args, **kwargs):
+        self.user = kwargs.pop('user')
+        super(ExistingUserApplicationForm, self).__init__(*args, **kwargs)
+
+    def save(self):
+        self.instance.user = self.user
+        return super().save()
+
+    class Meta:
+        model = CourseAssignment
+        fields = (
+            'course',
+            'group_time',
         )
 
 
