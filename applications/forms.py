934b2d36f4d9eacf0e3232c40d71aa17c8bccfa3
syndbg <syndbe@gmail.com>
2015-01-26 14:43:56 +0200

    Removed form social accounts validators. When the fields aren't required, the validators are useless.

--- applications/forms.py
+++ applications/forms.py
@@ -22,8 +22,8 @@ class ApplicationForm(forms.ModelForm):
     email = forms.EmailField(label='Email')
     skype = forms.CharField(label='Skype', max_length=100)
     phone = forms.CharField(label='Телефон', max_length=100)
-    github_account = forms.CharField(label='Github', widget=forms.TextInput(attrs={'placeholder': 'https://github.com/HackBulgaria'}), max_length=100, validators=[validate_github], required=False)
-    linkedin_account = forms.CharField(label='Linkedin', widget=forms.TextInput(attrs={'placeholder': 'https://www.linkedin.com/'}), max_length=100, validators=[validate_linkedin], required=False)
+    github_account = forms.CharField(label='Github', widget=forms.TextInput(attrs={'placeholder': 'https://github.com/HackBulgaria'}), max_length=100, required=False)
+    linkedin_account = forms.CharField(label='Linkedin', widget=forms.TextInput(attrs={'placeholder': 'https://www.linkedin.com/'}), max_length=100, required=False)
 
     def clean(self):
         cleaned_data = super(ApplicationForm, self).clean()
@@ -32,24 +32,22 @@ class ApplicationForm(forms.ModelForm):
 
         if User.is_existing(email):
             self.add_error('email', EMAIL_DUPLICATE_ERROR)
-
         if len(name.split(' ')) < 2:
             self.add_error('name', NAMES_ERROR)
+
         return cleaned_data
 
     def save(self):
         course = self.cleaned_data['course']
         course_name = course.name
         email = self.cleaned_data['email']
-        github_account = self.cleaned_data.get('github_account', '')
-        linkedin_account = self.cleaned_data.get('linkedin_account', '')
         name = self.cleaned_data['name']
         password = User.objects.make_random_password()
 
         new_user = User.objects.create_user(email=email, password=password)
         new_user.set_full_name(name)
-        new_user.linkedin_account = linkedin_account
-        new_user.github_account = github_account
+        new_user.linkedin_account = self.cleaned_data.get('linkedin_account', '')
+        new_user.github_account = self.cleaned_data.get('github_account', '')
         new_user.save()
 
         self.instance.student = new_user
