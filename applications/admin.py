153bdce966e6e047946a382f8e660da635b6ad51
syndbg <syndbe@gmail.com>
2015-01-28 20:12:36 +0200

    Updated admit_applications command

--- applications/admin.py
+++ applications/admin.py
@@ -5,9 +5,11 @@ from django.contrib.admin import helpers
 from django.core.mail import send_mass_mail
 from django.template import Context, Template
 from django.template.loader import get_template
+from django.template.loader import render_to_string
 from django.template.response import TemplateResponse
 
 from applications.models import Application, ApplicationTask, ApplicationSolution
+from students.models import CourseAssignment
 
 
 class ApplicationAdmin(admin.ModelAdmin):
@@ -20,23 +22,32 @@ class ApplicationAdmin(admin.ModelAdmin):
     list_filter = ('course',)
 
     def admit_applications(self, request, queryset):
+
         if request.POST.get('post'):
             n = queryset.count()
             if n:
                 mails = []
                 form_message_template = request.POST.get('message_template')
                 template = Template(form_message_template) if form_message_template else get_template('admit_template.html')
+                courses = [obj.course for obj in queryset]
+                admitted_students = CourseAssignment.objects.filter(course__in=courses).only('user')
 
                 for obj in queryset:
-                    course = obj.course
                     student = obj.student
-
+                    if student in admitted_students:
+                        continue
+                    course = obj.course
+                    CourseAssignment.objects.create(
+                        course=course,
+                        user=student,
+                        group_time=CourseAssignment.EARLY,
+                        )
                     context = Context({
-                        'course': obj.course,
+                        'course': course,
                         'course_name': course.name,
                         'course_start_date': course.start_time,
-                        'student': obj.student,
-                        'student_name': obj.student.get_full_name(),
+                        'student': student,
+                        'student_name': student.get_full_name(),
                     })
                     message = template.render(context)
                     subject = 'HackBulgaria admission for {0}'.format(course)
@@ -51,6 +62,7 @@ class ApplicationAdmin(admin.ModelAdmin):
                     dj_messages.SUCCESS)
             return None
 
+        default_text = render_to_string('admit_template.html')
         opts = self.model._meta
         if len(queryset) == 1:
             objects_name = opts.verbose_name
@@ -60,6 +72,7 @@ class ApplicationAdmin(admin.ModelAdmin):
         context = {
             'title': "Are you sure?",
             'objects_name': objects_name,
+            'default_text': default_text,
             'queryset': queryset,
             'action_checkbox_name': helpers.ACTION_CHECKBOX_NAME,
         }
